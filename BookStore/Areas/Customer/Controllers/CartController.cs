06c1fa1975dae48f92e943416eb5841f7c8a0bf9
Tarek Mamdouh Mostafa <egypty@hotmail.com>
2023-07-22 18:20:56 +0300

    next update

--- BookStore/Areas/Customer/Controllers/CartController.cs
+++ BookStore/Areas/Customer/Controllers/CartController.cs
@@ -29,8 +29,29 @@ namespace BookStore.Areas.Customer.Controllers
 				.Where(u => u.ApplicationUserId == claim.Value),
 				
 			};
+			foreach (var cart in ShoppingCartVM.ListCart)
+			{
+				cart.Price = GetPriceBasedOnQuantity(cart.Count, cart.Product.Price,
+					cart.Product.Price50, cart.Product.Price100);
+				
+			}
 
 			return View(ShoppingCartVM);
         }
-    }
+		private double GetPriceBasedOnQuantity(double quantity, double price, double price50, double price100)
+		{
+			if (quantity <= 50)
+			{
+				return price;
+			}
+			else
+			{
+				if (quantity <= 100)
+				{
+					return price50;
+				}
+				return price100;
+			}
+		}
+	}
 }
