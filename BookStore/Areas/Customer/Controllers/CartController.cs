ed7bbaa8b9fa233faf9aad015405a9075b4457c2
Tarek Mamdouh Mostafa <egypty@hotmail.com>
2023-07-24 02:53:48 +0300

    Shopping Cart now is about to go to create an Order

--- BookStore/Areas/Customer/Controllers/CartController.cs
+++ BookStore/Areas/Customer/Controllers/CartController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
+using Utility;
 
 namespace BookStore.Areas.Customer.Controllers
 {
@@ -26,20 +27,75 @@ namespace BookStore.Areas.Customer.Controllers
 			{
 				ListCart = _db.ShoppingCarts
 				.Include(s => s.Product)
-				.Where(u => u.ApplicationUserId == claim.Value),
-				
+				.Where(u => u.ApplicationUserId == claim.Value),				
 			};
 			foreach (var cart in ShoppingCartVM.ListCart)
 			{
 				cart.Price = GetPriceBasedOnQuantity(cart.Count, cart.Product.Price,
 					cart.Product.Price50, cart.Product.Price100);
                 ShoppingCartVM.CartTotal += (cart.Price * cart.Count);
-
-
             }
-
             return View(ShoppingCartVM);
         }
+
+		public IActionResult Summary()
+		{
+			var claimsIdentity = (ClaimsIdentity)User.Identity;
+			var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+			ShoppingCartVM = new ShoppingCartVM()
+			{
+				ListCart = _db.ShoppingCarts
+				.Include(s => s.Product)
+				.Where(u => u.ApplicationUserId == claim.Value),				
+			};
+
+			foreach (var cart in ShoppingCartVM.ListCart)
+			{
+				cart.Price = GetPriceBasedOnQuantity(cart.Count, cart.Product.Price,
+					cart.Product.Price50, cart.Product.Price100);
+				ShoppingCartVM.CartTotal += (cart.Price * cart.Count);
+			}
+			return View(ShoppingCartVM);
+		}
+
+		public IActionResult Plus(int cartId)
+		{
+			var cart = _db.ShoppingCarts.FirstOrDefault(u => u.Id == cartId);
+			cart.Count++;
+			_db.ShoppingCarts.Update(cart);
+			_db.SaveChanges();
+			return RedirectToAction(nameof(Index));
+		}
+
+		public IActionResult Minus(int cartId)
+		{
+			var cart = _db.ShoppingCarts.FirstOrDefault(u => u.Id == cartId);
+			if (cart.Count <= 1)
+			{
+				_db.ShoppingCarts.Remove(cart);
+				var count = _db.ShoppingCarts
+					.Where(u => u.ApplicationUserId == cart.ApplicationUserId).ToList().Count - 1;				
+			}
+			else
+			{
+				cart.Count--;
+				_db.ShoppingCarts.Update(cart);
+			}
+			_db.SaveChanges();
+			return RedirectToAction(nameof(Index));
+		}
+
+		public IActionResult Remove(int cartId)
+		{
+			var cart = _db.ShoppingCarts.FirstOrDefault(u => u.Id == cartId);
+			_db.ShoppingCarts.Remove(cart);
+			_db.SaveChanges();
+
+			var count = _db.ShoppingCarts.Select(u => u.ApplicationUserId == cart.ApplicationUserId).ToList().Count;			
+			return RedirectToAction(nameof(Index));
+		}
+
 		private double GetPriceBasedOnQuantity(double quantity, double price, double price50, double price100)
 		{
 			if (quantity <= 50)
