3a9e6ef956459bc30aa81e0c735cb0f5cb0db663
tarekmostafa <egypty3@gmail.com>
2024-07-14 04:39:47 +0300

    + Sending emails from the app using mailkit and mimekit packages

--- Utility/EmailSender.cs
+++ Utility/EmailSender.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.UI.Services;
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Mail;
+
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +12,23 @@ namespace Utility
 {
     public class EmailSender : IEmailSender
     {
-         public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
+            var emailToSend = new MimeMessage();
+            emailToSend.From.Add(MailboxAddress.Parse("hello@dotnetmastery.com"));
+            emailToSend.To.Add(MailboxAddress.Parse(email));
+            emailToSend.Subject = subject;
+            emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage };
+
+
+            //send email
+            using (var emailClient = new SmtpClient())
+            {
+                emailClient.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+                emailClient.Authenticate("dotnetmastery@gmail.com", "DotNet123$");
+                emailClient.Send(emailToSend);
+                emailClient.Disconnect(true);
+            }
             return Task.CompletedTask;
         }
     }
