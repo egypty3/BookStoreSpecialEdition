47abd8f7a68b79e5d0152ed5cd1902044ff578d0
Tarek Mamdouh <egypty3@gmail.com>
2024-07-18 20:46:15 +0300

    + I have modified the Email sender to be Fake sener !!!

--- Utility/EmailSender.cs
+++ Utility/EmailSender.cs
@@ -14,21 +14,21 @@ namespace Utility
     {
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            var emailToSend = new MimeMessage();
-            emailToSend.From.Add(MailboxAddress.Parse("hello@dotnetmastery.com"));
-            emailToSend.To.Add(MailboxAddress.Parse(email));
-            emailToSend.Subject = subject;
-            emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage };
+            //var emailToSend = new MimeMessage();
+            //emailToSend.From.Add(MailboxAddress.Parse("hello@dotnetmastery.com"));
+            //emailToSend.To.Add(MailboxAddress.Parse(email));
+            //emailToSend.Subject = subject;
+            //emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage };
 
 
-            //send email
-            using (var emailClient = new SmtpClient())
-            {
-                emailClient.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
-                emailClient.Authenticate("dotnetmastery@gmail.com", "DotNet123$");
-                emailClient.Send(emailToSend);
-                emailClient.Disconnect(true);
-            }
+            ////send email
+            //using (var emailClient = new SmtpClient())
+            //{
+            //    emailClient.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+            //    emailClient.Authenticate("dotnetmastery@gmail.com", "DotNet123$");
+            //    emailClient.Send(emailToSend);
+            //    emailClient.Disconnect(true);
+            //}
             return Task.CompletedTask;
         }
     }
